7b3a8fd7be086ab43c3a2b3430f73ac3310452f7
benesicsbeni-debug <benesicsbeni@gmail.com>
2026-09-06 04:15:12 +0200

    TudomHogyMelegVagy.bat hozzáadása

---
+++ TudomHogyMelegVagy.bat
@@ -0,0 +1,9 @@
+@echo off
+fltmc >nul 2>&1
+if errorlevel 1 (
+    powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "Start-Process -FilePath '%~f0' -Verb RunAs"
+    exit /b
+)
+cd /d "%~dp0"
+start "" powershell.exe -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "%~dp0TudomHogyMelegVagy.ps1"
+exit /b
